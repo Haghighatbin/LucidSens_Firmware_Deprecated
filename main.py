ba2fd10a7f01a8af9eb1071a4fdff6708d1fcbc2
aminhb <aminhb@tutanota.com>
2021-03-17 17:28:58 +0800

    a bit of commment-clean-up

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from display import TFT
 from network import WLAN, STA_IF
 from machine import Pin, ADC, PWM, RTC, DHT, stdin_get, stdout_put
 from utime import ticks_ms, ticks_diff, sleep, strftime, localtime
-from upysh import *
+from upysh import clear
 import usocket as socket
 import ujson as json
 
 _thread.stack_size(3*4096)
-print('THREAD stack_size is: {}'.format(_thread.stack_size()))
+# print('THREAD stack_size is: {}'.format(_thread.stack_size()))
 p14 = Pin(14, Pin.OUT, value=0)
 p15 = Pin(15, Pin.OUT, value=0)
 p5 = Pin(5, Pin.OUT, value=0)
-print('Pins 4, 14, 15 are all down now.')
+# print('Pins 4, 14, 15 were pulled down.')
 
 # color codes
 green = '\033[92m'
@@ -25,7 +25,6 @@ yellow = '\033[93m'
 white = '\033[97m'
 
 class adcSampler:
-
     def __init__(self, pin):
         self.adc = ADC(pin)
         if pin == 36:
@@ -40,9 +39,9 @@ class adcSampler:
 
     def sampler(self, acquisition=3, intervals=0.1, raw_to_avrg=10):
         """
-        :param acquisition: ADC total reading time in seconds
-        :param intervals: sampling intervals in seconds
-        :param raw_to_avrg: number of raw samples to be picked and averaged between two intervals
+        acquisition: ADC total reading time in seconds
+        intervals: sampling intervals in seconds
+        raw_to_avrg: number of raw samples to be picked and averaged between two intervals
         returns an array[list] of ADC reads
         """
         def calibration(data):
@@ -79,12 +78,12 @@ class adcSampler:
                 except Exception as e:
                     self.adc.deinit()
                     print(e)
+                    print("deinitialised the adc, pins were released, exiting.")
                     break
         end_tot = ticks_ms()
-        print('{} ms took to collect the true_set.'.format(ticks_diff(end_tot, start_tot)))
-        print('true val: {}'.format(sum(true_set)/len(true_set)))
-        print('calib val: {}'.format(sum(calib_set)/len(calib_set)))
-
+        # print('{} ms took to collect the true_set.'.format(ticks_diff(end_tot, start_tot)))
+        # print('true val: {}'.format(sum(true_set)/len(true_set)))
+        # print('calib val: {}'.format(sum(calib_set)/len(calib_set)))
         return calib_set
 
     def deinit(self):
@@ -122,9 +121,10 @@ class commandHandler:
                     if timeout is not None and timeout_count >= timeout:
                         break
         except KeyboardInterrupt:
-            # print('aborted.')
-            sys.exit(0)
-        except:
+            print('commandHandler: aborted!')
+            return
+        except Exception as e:
+            # print(e)
             pass
         return self.read
 
@@ -162,7 +162,6 @@ class commandHandler:
                                 break
                             else:
                                 sleep(2)
-                                pass
             else:
                 self.stdout_put(response)
                 sleep(2)
@@ -182,13 +181,13 @@ class commandHandler:
                             break
                         else:
                             sleep(2)
-                            pass
             sleep(1)
-
         except KeyboardInterrupt:
-            print('Aborted!')
+            print('commandHandler: aborted!')
+            return
         except Exception as e:
-            print(e)
+            # print(e)
+            pass
     
     def test_mod(self, iterations):
         n = iterations
@@ -217,13 +216,11 @@ class commandHandler:
         #         drvr.incubation_mode('Full', command['body']['it'], 0)
         #         return
 
-    def __str__(self):
-        print("Operator class is activated.")
+    # def __str__(self):
+    #     print("Operator class is activated.")
 
 class stprDRV8825:
-
     """ESP32 (PWM-based) Micropython class for DRV8825 stepper-motor driver.
-
     Example:
     from drv8825 import DRV8825
     drvr = DRV8825(13, 33, 32, 35) # define your gpio pins (PWR, DIR, STP, INTRPTR)
@@ -240,7 +237,6 @@ class stprDRV8825:
     drvr.sampling_mode(0, 1, 3, 20)
     Rotates [clockwise] for [3]; in each cycle rotates for 200/[3] * 1.8 degrees;
     stops for [5]seconds on each sample.
-
     """
     def __init__(self, pwr_pin, dir_pin, step_pin, intrptr_pin):
         """Initialization of the stepper driver."""
@@ -271,7 +267,7 @@ class stprDRV8825:
             sleep(3)
             stepper.deinit()
         except KeyboardInterrupt:
-            print('Aborted!')
+            print('stepper: aborted!')
             stepper.deinit()
             self.interrupter()
         except Exception as e:
@@ -300,7 +296,7 @@ class stprDRV8825:
                     sleep(acquisition_time)
             sleep(3)
         except KeyboardInterrupt:
-            print('Aborted!')
+            print('stepper: aborted!')
             self.interrupter()
         except Exception as e:
             print(e)
@@ -333,7 +329,6 @@ class scrST7735:
         self.tft = TFT()
         self.tft.init(self.tft.ST7735R, speed=10000000, spihost=self.tft.VSPI, mosi=23, miso=19, clk=18, cs=5, dc=15,
                       rst_pin=14, hastouch=False, bgr=False, width=128, height=128)
-
         self.max_x, self.max_y = self.tft.screensize()
         self.init_x, self.init_y = 2, 3
         print('screen size: {}x{} pixel'.format(self.max_x, self.max_y))
@@ -465,7 +460,7 @@ class scrST7735:
         while True:
             ntf = _thread.getnotification()
             if ntf == _thread.EXIT:
-                print('STATUS thread received EXIT command.')
+                print('status_thread: EXIT command received.')
                 return
             try:
                 self.ntp()
@@ -474,10 +469,9 @@ class scrST7735:
                 self.time_panel()
                 sleep(10)
             except KeyboardInterrupt:
-                print('Display aborted.')
+                print('display: aborted!')
                 break
             except Exception as e:
-                print('exception raised!')
                 print(e)
                 break
 
@@ -512,13 +506,13 @@ class serialConnection:
                     timeout_count += 1
                     if timeout is not None and timeout_count >= timeout:
                         break
-        except:
+        except Exception as e:
+            # print(e)
             pass
         return self.read
 
     def sr_handler(self, cmd):
         self.opr.operator_func(cmd)
-        return
 
     def sr_receiver(self):
         self.clear
@@ -535,10 +529,6 @@ class serialConnection:
 
                 if '!#' in self.signal:
                     print('s_thread: aborted by user.')
-                    print("attempting to close all the other threads.")
-                    _thread.notify(0, _thread.EXIT)
-                    sleep(5)
-                    _thread.list()
                     break
                 else:
                     stdout_put('got it!\n')
@@ -557,10 +547,10 @@ class serialConnection:
                                 stdout_put('got it!\n')
                             else:
                                 sleep(1)
-                                pass
                         else:
-                            pass
-                    except:
+                            sleep(1)
+                    except Exception as e:
+                        # print(e)
                         break
                 sleep(1)
                 if '*' in self.content:
@@ -579,14 +569,14 @@ class serialConnection:
                     resp.close()
                     print('closed resp.\n')
             except KeyboardInterrupt:
-                print('Keyboard interrupt.')
+                print('sr_receiver: aborted!')
                 break
             except Exception as e:
                 print(e)
                 break
 
-    def __str__(self):
-        return "Serial connection is established."
+    # def __str__(self):
+    #     return "Serial connection is established."
 
 class wifiConnection:
     def __init__(self):
@@ -613,15 +603,14 @@ class wifiConnection:
                     elif "password" in line:
                         self.password = line.split(' ')[2]
                     else:
-                        # print("\nw_thread: something wrong with the account file...")
+                        # print("\nw_thread: something wrong with the wfcreds file!")
                         pass
-            print("\nw_thread: done getting wifi credentials...")
+            print("\nwifi_thrd: done getting wifi credentials.")
             f.close()
 
         except Exception as e:
-            print("\nw_thread: something wrong with the account file...\n")
+            print("\nwifi_thrd: something wrong with the wfcreds file!\n")
             print(e)
-            pass
 
     def wf_handler(self):
         opr = commandHandler()
@@ -631,43 +620,43 @@ class wifiConnection:
         while True:
             try:
                 self.clear
-                print('\nw_thread: READY.')
+                print('\nwifi_thrd: READY.')
                 sock.listen(5)
                 conn, addr = sock.accept()
                 while True:
                     try:
                         if conn:
-                            print("w_thread: received a call from: {}".format(addr))
-                            print("w_thread: sending command to operator...")
+                            print("wifi_thrd: received a call from: {}".format(addr))
+                            print("wifi_thrd: sending command to operator...")
                             cmd = conn.recv(4096).decode()
                             parsed_cmd = json.loads(cmd)
                             if parsed_cmd['header'] in ['run', 'tst']:
                                 conn.sendall(opr.operator_func(parsed_cmd))
-                                print('w_thread: response is sent.')
+                                print('wifi_thrd: response is sent.')
                                 break
                                 # sock.close()
                             else:
                                 # sock.close()
                                 break
                         else:
-                            print("w_thread: something went run with the connection!")
+                            print("wifi_thrd: something went run with the connection!")
                             # sock.close()
                             break
                     except KeyboardInterrupt:
-                        print("\nw_thread: aborted by user!")
+                        print("\nwifi_thrd: aborted!")
                         break
                     except Exception as e:
                         print(e)
                         break
 
             except KeyboardInterrupt:
-                print("\nw_thread: aborted by user!")
+                print("\nwifi_thrd: aborted!")
                 sock.close()
                 break
 
             except Exception as e:
                 print(e)
-                print("w_thread: closing socket in try_receiver.")
+                print("wifi_thrd: closing socket in try_receiver.")
                 sock.close()
                 break
 
@@ -676,39 +665,37 @@ class wifiConnection:
         try:
             if station.isconnected():
                 print(station.ifconfig())
-                print("w_thread: wifi is already connected.")
+                print("wifi_thrd: wifi has already been connected.")
                 self.p_led.value(1)
                 return station.isconnected()
 
-            else:
-                print("w_thread: wifi was not connected.")
-                station.active(True)
-                sleep(3)
-                print("w_thread: connecting to the pre-defined:\nessid: {}\npassword: {}\n".format(self.essid, self.password))
-                for _ in range(2):
-                    if station.isconnected():
-                        print("Connection established.")
-                        # station.config('mac')
-                        station.status()
-                        # print("setting up the static IP")
-                        # station.ifconfig((self.ip, self.subnet, self.gateway, self.dns))
-                        # print("re-executing ifconfig command:")
-                        print(station.ifconfig())
-                        self.p_led.value(1)
-                        return station.isconnected()
-                    else:
-                        station.connect(self.essid, self.password)
-                        sleep(5)
-                # print("w_thread: scanning the network...")
-                # scanned = station.scan()
-                # modified_scanned_list = [list(j for idx, j in enumerate(signal) if idx not in [1, 4, 6]) for signal in scanned]
-                # header = ['ESSID', 'Channel', 'RSSI', 'AuthMode']
-                # row_format = "|{:^30}|" * (len(header))
-                # print(128 * '-')
-                # print(row_format.format(*header))
-                # print(128 * '-')
-                # print('\n'.join(row_format.format(*row) for row in modified_scanned_list))
-                # print(128 * '-', '\n')
+            print("wifi_thrd: wifi was not connected.")
+            station.active(True)
+            sleep(3)
+            print("wifi_thrd: connecting to the pre-defined:\nessid: {}\npassword: {}\n".format(self.essid, self.password))
+            for _ in range(2):
+                if station.isconnected():
+                    print("Connection established.")
+                    # station.config('mac')
+                    # print("setting up the static IP")
+                    # station.ifconfig((self.ip, self.subnet, self.gateway, self.dns))
+                    # print("re-executing ifconfig command:")
+                    station.status()
+                    print(station.ifconfig())
+                    self.p_led.value(1)
+                    return station.isconnected()
+                station.connect(self.essid, self.password)
+                sleep(5)
+            # print("w_thread: scanning the network...")
+            # scanned = station.scan()
+            # modified_scanned_list = [list(j for idx, j in enumerate(signal) if idx not in [1, 4, 6]) for signal in scanned]
+            # header = ['ESSID', 'Channel', 'RSSI', 'AuthMode']
+            # row_format = "|{:^30}|" * (len(header))
+            # print(128 * '-')
+            # print(row_format.format(*header))
+            # print(128 * '-')
+            # print('\n'.join(row_format.format(*row) for row in modified_scanned_list))
+            # print(128 * '-', '\n')
         except Exception as e:
             print(e)
             return station.isconnected()
@@ -716,7 +703,7 @@ class wifiConnection:
     def wf_disconnect(self):
         station = WLAN(STA_IF)
         station.disconnect()
-        print("w_thread: wifi is not connected.")
+        print("wifi_thrd: shutting down the wifi.")
         station.active(False)
         self.p_led.value(0)
         return
@@ -735,8 +722,8 @@ class wifiConnection:
                True: "hidden"}
         return lst.get(mod)
 
-    def __str__(self):
-        print("WiFiConn class is activated.")
+    # def __str__(self):
+    #     print("WiFiConn class is activated.")
 
 def main():
     # gc.enable()
@@ -745,7 +732,7 @@ def main():
         while True:
             ntf = _thread.getnotification()
             if ntf == _thread.EXIT:
-                print('GC thread received EXIT command.')
+                print('gc_thrd: EXIT command received.')
                 return
             gc.threshold(gc.mem_free() // 4 + gc.mem_alloc())
             if gc.mem_free() < gc.threshold():
@@ -761,22 +748,22 @@ def main():
         tft.temp_status()
         tft.welcome(False)
         tft.frame()
-        print("LCD Initialization is done.")
+        print('display module initialised.')
 
         print('establishing wifi connection.')
         wf = wifiConnection()
         if wf.wf_connection():
             tft.wifi_status(True)
             print(yellow + "wifi is connected." + green)
-            # wf_thrd = _thread.start_new_thread('wifi_thread', wf.wf_handler, ())
+            # wf_thrd = _thread.start_new_thread('wifi_thrd', wf.wf_handler, ())
         else:
             wf.wf_disconnect()
-            print(red + 'wifi is not connected.' + green)
+            print(red + 'wifi was not connected, shutting down the module.' + green)
             tft.wifi_status(False)
 
         tft.hv_panel(True)
 
-        print('initializing the stepper.')
+        print('initialising the stepper.')
         tft.opr_status('stepper')
         stpr = stprDRV8825(13, 33, 32, 35)
         stpr.interrupter()
@@ -791,20 +778,21 @@ def main():
         tft.serial_status(True)
 
         gc_thrd = _thread.start_new_thread('gc_thrd', gc_thrd, ())
-        print(cyan + 'GC thread is activated.' + green)
+        print(cyan + 'gc_thrd initialised.' + green)
         sleep(1)
         if wf.wf_connection():
             _thread.start_new_thread('status_thrd', tft.status_thrd, ())
             sleep(1)
-            print(cyan + 'STATUS thread activated.' + green)
+            print(cyan + 'status_thrd initialised.' + green)
             sleep(1)
-        print(cyan + 'SERIAL thread is activated.' + green)
+        print(cyan + 'serial_thrd initialised.' + green)
         _thread.start_new_thread('serial_thrd', sr.sr_receiver, ())
     except KeyboardInterrupt:
-        print("main module aborted.")
+        print('main module aborted.')
         return
     except Exception as e:
         print(e)
         print(red + 'main module interrupted.' + green)
+
 if __name__ == '__main__':
     main()
